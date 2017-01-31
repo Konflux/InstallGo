1d243475b1c07732bb0606f4715b8418488a6655
Marius Norli <Marius Norli>
2017-01-31 18:26:19 +0100

    fiks

--- InstalGo.sh
+++
@@ -1,9 +0,0 @@
-CLIENT_DIRECTORY=/home/Go/Work
-export GOPATH=$CLIENT_DIRECTORY
-mkdir -p $CLIENT_DIRECTORY
-cd $CLIENT_DIRECTORY
-mkdir bin
-mkdir pkg
-mkdir src
-echo Your GOPATH has been set to:
-printenv GOPATH
